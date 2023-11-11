5eb2e3b26405a41bd9dd856097d24c72f746ef13
sophiahdew <sophiahdew@gmail.com>
2023-11-11 06:21:42 +0300

    fixed rendering

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,64 @@ import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import CommonAds from 'src/common-ads'
 import { parseEther } from "viem";
 
+const styles = {
+  container: {
+    textAlign: 'left',
+    padding: '20px',
+    fontFamily: 'Arial, sans-serif',
+  },
+  button: {
+    backgroundColor: '#4CAF50', // Green
+    border: 'none',
+    color: 'white',
+    padding: '15px 32px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '16px',
+    margin: '4px 2px',
+    cursor: 'pointer',
+  },
+  input: {
+    padding: '12px 20px',
+    margin: '8px 0',
+    display: 'block', // Changed to block to stack vertically
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    boxSizing: 'border-box',
+    width: '100%', // Set width to 100% to fill the parent
+  },
+  inputContainer: {
+    marginBottom: '20px', // Add some space below the container
+  },
+  connectedInfo: {
+    backgroundColor: '#f2f2f2',
+    padding: '10px',
+    borderRadius: '5px',
+    marginBottom: '10px',
+  },
+  profileContainer: {
+    position: 'absolute', // Positioning relative to the first positioned ancestor
+    top: 0, // Align to the top of the container
+    right: 0, // Align to the right of the container
+    padding: '10px', // Add some space from the edges of the screen
+  },
+  gridContainer: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', // This will create a responsive grid with a minimum column width of 250px
+    gridGap: '20px', // This is the gap between the cards
+    padding: '20px', // Add padding around the entire grid for better spacing from the container's edges
+  },
+  card: {
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+    transition: '0.3s',
+    borderRadius: '5px', // use var(--border-radius) if you want to be consistent with global styles
+    padding: '2px 16px',
+    margin: '16px',
+    textAlign: 'center',
+  }
+};
+
 //checks for users wallet connection
 function Profile() {
   const { address, isConnected } = useAccount()
@@ -21,12 +79,12 @@ function Profile() {
 
   if (isConnected)
     return (
-      <div>
+      <div style={styles.connectedInfo}>
         Connected to {address}
-        <button onClick={() => disconnect()}>Disconnect</button>
+        <button style={styles.button} onClick={() => disconnect()}>Disconnect</button>
       </div>
     )
-  return <button onClick={() => connect()}>Connect Wallet</button>
+  return <button style={styles.button} onClick={() => connect()}>Connect Wallet</button>
 }
 
 
@@ -39,10 +97,36 @@ export default function Home() {
   const [spaceId, setSpaceId] = useState(0);
   const [spaceData, setSpaceData] = useState(null);
 
+  const [claims, setClaims] = useState([]); // Added state to track claims
+  const [filter, setFilter] = useState(''); // Holds the current filter
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setMetadata((prevState) => ({ ...prevState, [name]: value }));
   };
+
+  // Define your options for the work scope dropdown
+  const workScopeOptions = [
+    "Research & Development",
+    "Community Service",
+    "Education",
+    "Healthcare",
+    "Technology",
+    "Environment",
+    "Longevity"
+    // ... add other options as needed
+  ];
+
+  // Function to handle the filter change
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  // Filtered claims based on the selected work scope
+  const filteredClaims = claims.filter(claim => {
+    return filter === '' || claim.link === filter; // If filter is empty, return all claims, otherwise return claims that match the filter
+  });
+
 
   {/* Functions for creating a space */ }
 
@@ -60,10 +144,16 @@ export default function Home() {
     args: [0, prices]
   })
 
-  const handleCreateSpace = async () => {
+  const handleCreateCard = async () => {
     console.log('create space button clicked')
+    // await writeMetadata();
+    // await createSpace();
+    setClaims(prevClaims => [...prevClaims, metadata]); // Add new claim to the list
+    // Now call your blockchain functions to store the claim
     await writeMetadata();
     await createSpace();
+    // Optionally reset metadata state here if you want the input fields to clear after submission
+    setMetadata({ name: "", desc: "", link: "", img: "" });
   }
 
   {/* Functions for fetching a space */ }
@@ -110,33 +200,74 @@ export default function Home() {
     //also need to get existing price from getting space
   }
 
-
   return (
-    <div style={{ textAlign: "center" }}>
-
-      <h1>Open Source Project Auction</h1>
-
-      <Profile />
-
-      <h2>Create a New Space</h2>
-
-      <div>
-        <input name="name" value={metadata.name} onChange={handleInputChange} placeholder="Name" />
-        <input name="desc" value={metadata.desc} onChange={handleInputChange} placeholder="Description" />
-        <input name="link" value={metadata.link} onChange={handleInputChange} placeholder="Link" />
-        <input name="img" value={metadata.img} onChange={handleInputChange} placeholder="Image URL" />
-        <button onClick={handleCreateSpace}>Submit Metadata</button>
+    <div style={styles.container}>
+      <div style={styles.profileContainer}>
+        <Profile />
+      </div>
+      <h1 style={{ paddingBottom: 30 }}>Fund your Research</h1>
+      {/* <Profile /> */}
+      <h2>Create an Impact Claim</h2>
+      <div style={styles.inputContainer}>
+        <input style={styles.input} name="name" value={metadata.name} onChange={handleInputChange} placeholder="Name of Impact" />
+        <input style={styles.input} name="desc" value={metadata.desc} onChange={handleInputChange} placeholder="Description" />
+        {/* <input style={styles.input} name="link" value={metadata.link} onChange={handleInputChange} placeholder="Work Scope" /> */}
+        {/* Dropdown for Work Scope */}
+        <select style={styles.input} name="link" value={metadata.link} onChange={handleInputChange}>
+          <option value="">Select Work Scope</option>
+          {workScopeOptions.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
+          ))}
+        </select>
+        <input style={styles.input} name="img" value={metadata.img} onChange={handleInputChange} placeholder="Project Image" />
+        <button style={styles.button} onClick={handleCreateCard}>Submit Claim</button>
       </div>
 
-      <h2>Fetch Space</h2>
+      {/* Dropdown for filtering cards based on work scope */}
+      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+        <label htmlFor="workScopeFilter">Filter by Work Scope:</label>
+        <select
+          id="workScopeFilter"
+          style={styles.input}
+          value={filter}
+          onChange={handleFilterChange}
+        >
+          <option value="">All Work Scopes</option>
+          {workScopeOptions.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
+          ))}
+        </select>
+      </div>
 
-      <input
+      {/* Grid container for cards */}
+      <div style={styles.gridContainer}>
+        {filteredClaims.map((claim, index) => (
+          <div key={index} style={styles.card}>
+            <h3>{claim.name}</h3>
+            <p>{claim.desc}</p>
+            <p>{claim.link}</p>
+            {/* Image could be displayed here if img is a URL */}
+            <img src={claim.img} alt={claim.name} style={{ width: '100%', height: 'auto', borderRadius: styles.card.borderRadius }} />
+            <p><button style={styles.button} onClick={() => handleBuy(claim)}>Buy Claim</button></p>
+          </div>
+        ))}
+      </div>
+
+
+
+
+      {/* <h2>Fetch Space</h2>
+      <input style={styles.input} placeholder="Space ID" onChange={(e) => setSpaceId(e.target.value)} />
+      <button style={styles.button} onClick={handleFetchSpace}>Fetch Space</button> */}
+
+
+      {/* <input
         placeholder="Space ID"
         onChange={(e) => setSpaceId(e.target.value)}
       />
-      <button onClick={handleFetchSpace}>Fetch Space</button>
+      <button onClick={handleFetchSpace}>Fetch Space</button> */}
 
-      {spaceId && (
+      {/* {spaceId && (
         <div>
           <h3>Owner: {spaceData[0]}</h3>
           <p>{spaceData[1].img}</p>
@@ -144,10 +275,9 @@ export default function Home() {
           <p>{spaceData[1].desc}</p>
           <p>{spaceData[1].link}</p>
           <button onClick={handleBuy}>Buy for ${prices[0]}</button>
-          {/* Render the rest of the space data here */}
-          {/* You can access spaceData.spaceMeta and spaceData.spots */}
         </div>
-      )}
+      )} */}
     </div>
   );
 }
+
